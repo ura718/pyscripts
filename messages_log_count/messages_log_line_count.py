656c493cfa3da515b23e3b37489f6f8c3b627979
root <root@kvmtest.bobst.nyu.edu>
2016-11-21 11:49:43 -0500

    Count how many lines occurred on a per minute basis in /var/log/messages

---
+++ messages_log_count/messages_log_line_count.py
@@ -0,0 +1,60 @@
+#!/opt/rh/python27/root/usr/bin/python
+
+
+#
+# Author: Yuri Medvinsky
+# Date: 11/21/2016
+# Extra: Need python 2.7 because collections.Counter is present starting from this version
+# Info: Read the /var/log/messages and print out Month Day Hours:Minutes, number_of_line_occurrences for that minute
+#
+# e.g:
+#    Nov 20 22:35,30
+#    Nov 20 22:36,2
+#    Nov 20 22:34,2
+#    Nov 21 10:38,1
+#
+
+
+import collections
+
+
+# Read messages file
+with open('/var/log/messages', 'r') as f:
+  f_lines = f.readlines()  
+
+
+
+# Declare empty array
+data = []
+
+
+
+
+for i in f_lines:
+
+  # The First join takes a line that is in list format and takes up to 2 columns 0,1 and joins it with spaces (e.g: Nov 20)
+  # The Second join takes a line that is in list format and takes a third column, splits it against colon (:) and takes first 2 columns and joins them with a colon(:) again (e.g 22:35:00 -> 22:35)
+  # Then we take the First join and concatenate it with Second join. We can concatenate only str() types and so when you do a join it converts a list into a str() automatically.
+
+  data.append(' '.join(i.strip('\n').split()[0:2]) + ' ' + ':'.join((i.strip('\n').split()[2]).split(':')[0:2]))
+
+
+
+# Count how many elements are similar in data array. We read this (e.g: for l in data do l.strip() count up same elements and assign to counts)
+counts = collections.Counter(l.strip() for l in data)
+
+
+
+# print count of same elements in the data array per line
+''' e.g:
+    Nov 20 22:35,30
+    Nov 20 22:36,2
+    Nov 20 22:34,2
+    Nov 21 10:38,1
+    Nov 21 10:39,1
+'''
+
+for line, count in counts.most_common():
+  print "%s,%s" % (line,count)
+
+
